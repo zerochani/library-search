3894ea37cafec2d37a3bfbb8d47f61e4d3ab4a9e
CHAN <150508884+zerochani@users.noreply.github.com>
2026-06-14 23:50:39 +0900

    initial commit

---
+++ src/test/java/com/library/librarysearch/LibrarySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.librarysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
